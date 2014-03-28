b916baa808716ac3212e0f9f8ca05434df656251
lucval <valtulina.luca@gmail.com>
2014-03-28 13:33:57 +0100

    Update script_18-19.sh

--- script_18-19.sh
+++ script_18-19.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+#############################################
+# download modified openflow switch support #
+# implementation, configure it and build it #
+#############################################
+
+git clone https://github.com/lucval/ofsid.git
+cd ofsid/
+./waf configure
+./waf build
+var=$(pwd)
+cd ../
+
+
+#############################################
+# configure ns-3 enabling openflow support  #
+#############################################
+
+./waf distclean
+./waf configure --with-openflow=$var ###add here further options if required
+cp -r ofsid/include/openflow/ build/
+
+
+#############################################
+# download modified lte, csma and openflow  #
+# modules in order to supporting dmm in ns3 #
+#############################################
+
 cd src/
 rm -rf lte
 git clone https://github.com/lucval/lte.git
@@ -12,6 +39,11 @@ git clone https://github.com/lucval/openflow.git
 
 rm csma/ipv4_17.h
 mv csma/ipv4.* internet/model
-
 cd ..
+
+
+#############################################
+#            finally build ns-3             #
+#############################################
+
 ./waf build
